e38cf03cec3ec7cd39ffab3f2604c9e758f7ea5c
Josiah Campbell <josiahcampbell64@gmail.com>
2017-01-06 14:42:29 -0500

    Add gradle files, runnable main class

---
+++ solution/src/main/java/edu/gvsu/cis/campbjos/connectfour/ConnectFourAI.java
@@ -0,0 +1,12 @@
+package edu.gvsu.cis.campbjos.connectfour;
+
+import java.util.Arrays;
+
+public class ConnectFourAI {
+
+    public static void main(String[] args) {
+        Arrays.asList(args).forEach(System.out::println);
+        System.exit(5);
+    }
+
+}
